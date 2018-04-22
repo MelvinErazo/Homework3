79c3e07300016463a7d2a65f6ed91fa578a2ed96
MelvinErazo <melvin.erazo@hotmail.com>
2018-04-22 00:16:22 -0600

    ejercicios
    solucion

---
+++ Código 2/Código 2 – Calcular el Peso – C#.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gvd=9.8;
+            double f,m;
+            Console.WriteLine("Calculador de peso Maquina Del Tiempo.\n");
+            Console.WriteLine("¿Ingrese la fuerza en NEWTON a la cual es sometida la maquina del tiempo?\n");
+            f = Convert.ToDouble(Console.ReadLine());
+            m = f / gvd;
+            Console.WriteLine("\n El peso en la maquina de tiempo es de: "+ Math.Round(m,2)+"kg");
+            Console.ReadKey();
+
+        }
+    }
+}
